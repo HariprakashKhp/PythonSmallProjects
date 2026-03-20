2ef77da6673ae078f022880fc91aab0f5c365b2d
hariprakashkhp <hariprakash.khp@gmail.com>
2026-03-20 21:16:54 +0530

    init commit for alien inv game

---
+++ alien_invastion_game/settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    def __init__(self):
+        self.width = 1200
+        self.height = 800
+        self.bg_color = (230, 230, 230)
+        self.game_name = "Alien Invasion"
+        self.frames = 60
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullet_limit = 3
